d11b9f300d987b7b9776fee209d98a1dc6345f69
AlexCFrost <siva.ganesh.c18@gmail.com>
2023-12-16 15:15:12 +0530

    added successful

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-// App.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import './App.css';
 
 function App() {
+  const [successMessage, setSuccessMessage] = useState(null);
+
   const formik = useFormik({
     initialValues: {
       firstName: '',
@@ -21,74 +22,73 @@ function App() {
     }),
     // For submit
     onSubmit: (values) => {
-      alert('Registration successful!');
+      setSuccessMessage('Registration successful!');
       console.log(values);
     },
   });
 
   return (
     <div className='all'>
+      {successMessage && <div className="success-message">{successMessage}</div>}
       <form onSubmit={formik.handleSubmit}>
-      <div>
-        <label htmlFor="firstName">FirstName:</label>
-        <input
-          type="text"
-          id="firstName"
-          name="firstName"
-          onChange={formik.handleChange}
-          value={formik.values.firstName}
-        />
-        {formik.touched.firstName && formik.errors.firstName ? (
-          <div>{formik.errors.firstName}</div>
-        ) : null}
-      </div>
+        <div>
+          <label htmlFor="firstName">FirstName:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            onChange={formik.handleChange}
+            value={formik.values.firstName}
+          />
+          {formik.touched.firstName && formik.errors.firstName ? (
+            <div>{formik.errors.firstName}</div>
+          ) : null}
+        </div>
 
-      <div>
-        <label htmlFor="lastName">Last Name:</label>
-        <input
-          type="text"
-          id="lastName"
-          name="lastName"
-          onChange={formik.handleChange}
-          value={formik.values.lastName}
-        />
-        {formik.touched.lastName && formik.errors.lastName ? (
-          <div>{formik.errors.lastName}</div>
-        ) : null}
-      </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            onChange={formik.handleChange}
+            value={formik.values.lastName}
+          />
+          {formik.touched.lastName && formik.errors.lastName ? (
+            <div>{formik.errors.lastName}</div>
+          ) : null}
+        </div>
 
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="text"
-          id="email"
-          name="email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}
-        />
-        {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
-      </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+        </div>
 
-      <div>
-        <label htmlFor="contacts">Contacts:</label>
-        <input
-          type="text"
-          id="contacts"
-          name="contacts"
-          onChange={formik.handleChange}
-          value={formik.values.contacts}
-        />
-        {formik.touched.contacts && formik.errors.contacts ? (
-          <div>{formik.errors.contacts}</div>
-        ) : null}
-      </div>
-      <button type="submit">Register</button>
-    </form>
+        <div>
+          <label htmlFor="contacts">Contacts:</label>
+          <input
+            type="text"
+            id="contacts"
+            name="contacts"
+            onChange={formik.handleChange}
+            value={formik.values.contacts}
+          />
+          {formik.touched.contacts && formik.errors.contacts ? (
+            <div>{formik.errors.contacts}</div>
+          ) : null}
+        </div>
+        <button type="submit">Register</button>
+      </form>
     </div>
-    
   );
 }
 
 export default App;
-
